3caa1fcffb36c9de84e1cf9b29a1d1924dbfb1fb
James Da Costa <mrjamesdacosta@gmail.com>
2014-01-22 21:23:07 +0000

    updated database settings

--- imagebase/settings.py
+++ imagebase/settings.py
@@ -59,7 +59,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'imagebase',
-        'USER': 'imagebase_user',
+        'USER': 'jamesdacosta',
         'PASSWORD': 'password',
         'HOST': '127.0.0.1',
         'PORT': '5432',
