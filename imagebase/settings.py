57b66a7b0f3020a5750c1e6ac804ba70c77023eb
James Da Costa <mrjamesdacosta@gmail.com>
2014-01-22 20:16:02 +0000

    added database

--- imagebase/settings.py
+++ imagebase/settings.py
@@ -57,8 +57,12 @@ WSGI_APPLICATION = 'imagebase.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'imagebase',
+        'USER': 'imagebase_user',
+        'PASSWORD': 'password',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
