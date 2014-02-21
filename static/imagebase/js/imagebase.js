9b69194a52bd02cc59129e056d0816224daec759
James Da Costa <mrjamesdacosta@gmail.com>
2014-02-21 16:57:07 +0000

    basic pjax for image detail page

---
+++ static/imagebase/js/imagebase.js
@@ -0,0 +1,35 @@
+// hacked jquery to add a sendDelay property
+$.ajaxSetup({
+    sendDelay: 2000,
+    timeout: 3000
+});
+
+var ImagebaseRouter = Backbone.Router.extend({
+
+    routes: {
+        'image/:id/': 'viewImageDetail'
+    },
+
+    viewImageDetail: function(id){
+        console.log('load image %s', id);
+        $('#image-detail-container').load('/image/'+id+'/');
+    }
+
+});
+
+
+$(function(){
+    // create our router instance
+    var imagebaseRouter = new ImagebaseRouter();
+
+    // intercept links with the data-internal attributes and navigate using the router
+    $('a[data-internal]').on('click', function(e){
+        e.preventDefault();
+        e.stopPropagation();
+        imagebaseRouter.navigate(e.currentTarget.pathname, {trigger: true});
+    });
+
+    // start listening to url changes (with pushState)
+    Backbone.history.start({pushState: true});
+});
+
