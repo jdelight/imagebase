90f3cffd9a356c13ad037210c32b33ca2e44ea09
James Da Costa <mrjamesdacosta@gmail.com>
2014-02-23 11:44:40 +0000

    readded

---
+++ static/imagebase/js/app.js
@@ -0,0 +1,37 @@
+var ImagebaseRouter = Backbone.Router.extend({
+
+    routes: {
+        '': 'viewDashboard',
+        'image/:id/': 'viewImage'
+    },
+
+    'viewDashboard': function(){
+        $('#image-detail-container').empty();
+        $('#image-master-container').removeClass('medium-8').addClass('medium-12');
+    },
+
+    'viewImage': function(id){
+        var imageContentUrl = '/image/' + id + '/content/';
+        console.log('load image %s from %s', id, imageContentUrl);
+        $('#image-detail-container').load(imageContentUrl, null, function(){
+            $('#image-master-container').removeClass('medium-12').addClass('medium-8');
+        });
+    }
+
+});
+
+
+$(function(){
+
+    var imagebaseRouter = new ImagebaseRouter();
+
+    $('a[data-pjax]').on('click', function(e){
+        e.preventDefault();
+        e.stopPropagation();
+        imagebaseRouter.navigate(e.currentTarget.pathname, {trigger: true});
+    });
+
+    Backbone.history.start({pushState: true, root:'/dashboard/'});
+
+});
+
