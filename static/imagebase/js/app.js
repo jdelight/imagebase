26e585132d5d645e329f95bba36ed85df4ca887a
James Da Costa <mrjamesdacosta@gmail.com>
2014-02-23 11:36:44 +0000

    updated

--- static/imagebase/js/app.js
+++ static/imagebase/js/app.js
@@ -2,8 +2,7 @@ var ImagebaseRouter = Backbone.Router.extend({
 
     routes: {
         '': 'viewDashboard',
-        'image/:id/': 'viewImage',
-        'image/:id/update/': 'updateImage'
+        'image/:id/': 'viewImage'
     },
 
     'viewDashboard': function(){
@@ -11,23 +10,9 @@ var ImagebaseRouter = Backbone.Router.extend({
         $('#image-master-container').removeClass('medium-8').addClass('medium-12');
     },
 
-    'updateImage': function(id){
-        console.log('update image %s:', id);
-        var imageUpdateUrl = '/image/' + id + '/update/content/';
-        var imageUrl = '/image/' + id + '/';
-        var router = this;
-
-        $('#modal').foundation('reveal', 'open', imageUpdateUrl);
-        $(document).on('closed', '[data-reveal]', function () {
-            router.navigate(imageUrl, {trigger: true});
-        });
-
-    },
-
     'viewImage': function(id){
         var imageContentUrl = '/image/' + id + '/content/';
         console.log('load image %s from %s', id, imageContentUrl);
-        $('#modal').foundation('reveal', 'close');
         $('#image-detail-container').load(imageContentUrl, null, function(){
             $('#image-master-container').removeClass('medium-12').addClass('medium-8');
         });
@@ -40,10 +25,9 @@ $(function(){
 
     var imagebaseRouter = new ImagebaseRouter();
 
-    $('main').on('click', 'a[data-pjax]', function(e){
+    $('a[data-pjax]').on('click', function(e){
         e.preventDefault();
         e.stopPropagation();
-        // console.log('e.currentTarget:', e.currentTarget);
         imagebaseRouter.navigate(e.currentTarget.pathname, {trigger: true});
     });
 
